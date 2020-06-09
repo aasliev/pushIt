c9dc375d4f6f6da880ca3eec8f2c5151a85c8bb5
Asliddin Asliev <you@example.com>
2020-06-09 16:00:35 -0400

    changed create new challenge design, added new controller, commonFunctions

--- PushIt/Controllers/createChallengeView.swift
+++ PushIt/Controllers/createChallengeView.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class createChallengeView: UIViewController {
+class createChallengeView: UIViewController, UITextViewDelegate {
     
    
 
     
     var calendar = Calendar.current
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let commonFunctions = CommonFunctions.sharedCommonFunction
 
     @IBOutlet weak var nameOfTheChallenge: UITextField!
     @IBOutlet weak var motivationText: UITextView!
@@ -25,31 +26,112 @@ class createChallengeView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         //let borderColor : UIColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         //motivationText.layer.borderWidth = 1.5
         //motivationText.layer.borderColor = borderColor.cgColor
         self.hideKeyboardWhenTappedAround()
         calendar.timeZone = TimeZone.current
+        self.navigationItem.hidesBackButton = true
+        
+        motivationText.delegate = self
+        // setting motivation textview placeholder
+        
+        motivationText.text = "Motivation"
+        motivationText.textColor = UIColor.lightGray
+
+        motivationText.becomeFirstResponder()
+
+        motivationText.selectedTextRange = motivationText.textRange(from: motivationText.beginningOfDocument, to: motivationText.beginningOfDocument)
+    }
+    // placeholder functions
+    
+    func textView(_ motivationText: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+
+        // Combine the textView text and the replacement text to
+        // create the updated text string
+        let currentText:String = motivationText.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+
+        // If updated text view will be empty, add the placeholder
+        // and set the cursor to the beginning of the text view
+        if updatedText.isEmpty {
+
+            motivationText.text = "Motivation"
+            motivationText.textColor = UIColor.lightGray
+
+            motivationText.selectedTextRange = motivationText.textRange(from: motivationText.beginningOfDocument, to: motivationText.beginningOfDocument)
+        }
+
+        // Else if the text view's placeholder is showing and the
+        // length of the replacement string is greater than 0, set
+        // the text color to black then set its text to the
+        // replacement string
+         else if motivationText.textColor == UIColor.lightGray && !text.isEmpty {
+            motivationText.textColor = UIColor.black
+            motivationText.text = text
+        }
+
+        // For every other case, the text should change with the usual
+        // behavior...
+        else {
+            return true
+        }
+
+        // ...otherwise return false since the updates have already
+        // been made
+        return false
     }
     
+    func textViewDidChangeSelection(_ motivationText: UITextView) {
+        if self.view.window != nil {
+            if motivationText.textColor == UIColor.lightGray {
+                motivationText.selectedTextRange = motivationText.textRange(from: motivationText.beginningOfDocument, to: motivationText.beginningOfDocument)
+            }
+        }
+    }
+
 
     //MARK: - Add new Challenge
-    
-    @IBAction func saveButtonPressed(_ sender: Any) {
+    @IBAction func CreateButtonPressed(_ sender: Any) {
         let tmpChallenge = Challenge(context: self.context)
         tmpChallenge.name = nameOfTheChallenge.text!
         tmpChallenge.motivation = motivationText.text!
         let dateStart = setHMS(date: dateStarted.date)
         print(dateStart)
         tmpChallenge.dateStart = dateStarted.date
-        
-        self.saveItem()
+        print(tmpChallenge.name)
+        print(tmpChallenge.motivation)
+        print(tmpChallenge.dateStart)
+        if tmpChallenge.name != "" && tmpChallenge.motivation != "Motivation" {
+            self.saveItem()
+            //performSegue(withIdentifier: "toChallengesView", sender: self)
+            _ = navigationController?.popViewController(animated: true)
+        }
+        else {
+            commonFunctions.createUIalert("Please fill out all the information", self)
+        }
+        //self.saveItem()
         print("------------")
         //print(dateStarted.date - date.timeIntervalSinceNow)
         
-        //performSegue(withIdentifier: "toChallengesView", sender: self)
-        _ = navigationController?.popViewController(animated: true)
     }
+    
+    
+    @IBAction func CancelButtonPressed(_ sender: Any) {
+        // pop uimessage, are you sure you want to cancel, and go back to the list of challenges
+        let alert : UIAlertController
+        
+        alert = UIAlertController(title: "Cancel", message: "Do you want to cancel?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "No", style: .cancel))
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+            _ = self.navigationController?.popViewController(animated: true)
+        }))
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
     
     func saveItem()
     {
