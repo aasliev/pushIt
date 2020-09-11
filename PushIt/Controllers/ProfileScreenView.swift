68ad17ffdf9b6f952fcaff36b4c716c0bddb5e8c
Asliddin Asliev <asliev.asliddin@gmail.com>
2020-09-10 20:49:55 -0400

    added sign out function

--- PushIt/Controllers/ProfileScreenView.swift
+++ PushIt/Controllers/ProfileScreenView.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ProfileScreenView: UIViewController {
+    
+    let progressBarInstance = SVProgressHUDClass.shared
+    let coreDataClassShared = CoreDataClass.sharedCoreData
+    let firestoreClassShared = FirebaseDatabase.shared
 
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var firstNameLastNameLabel: UILabel!
@@ -24,16 +28,45 @@ class ProfileScreenView: UIViewController {
         profilePicture.layer.borderWidth = 3
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toFriendScreen"{
-            self.navigationController?.popToRootViewController(animated: false)
-        }
+    
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+        let alert : UIAlertController
+        alert = UIAlertController(title: "Sing out", message: "Do you want to sign out?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "No", style: .cancel))
+        
+        //If user press 'yes', perform following functions
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+            
+           //this is for user on his/her profile screen.  This if function perform sign out procces
+            self.performSignOut()
+            //self.coreDataClassShared.coreDataUpdated = false
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
-
+    
     @IBAction func xButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
-        
     }
+    
+    
+    //MARK: Perform Sign Out
+    func performSignOut () {
+        self.progressBarInstance.displayProgressBar()
+        //This function call sign out user from Firebase Auth
+        FirebaseAuth.sharedFirebaseAuth.signOutCurrentUser()
+        self.coreDataClassShared.resetAllEntities()
+        self.navigationController?.navigationBar.isHidden = true;
+        // get a reference to the app delegate
+        let appDelegate: AppDelegate? = UIApplication.shared.delegate as? AppDelegate
+        // call didFinishLaunchWithOptions, this will make HomeScreen as Root ViewController
+        //Take user to Home Screen (Log In Screen), where user can log in.
+        appDelegate?.applicationDidFinishLaunching(UIApplication.shared)
+        self.progressBarInstance.dismissProgressBar()
+    }
+    
     
 }
